ce8b45d1b42437574fcf0fedeb7eb482b3245f5a
ramadhan <ramadhansahri158@gmail.com>
2020-12-07 21:00:23 +0700

    merubah index.jsx

--- src/containers/comics/index.jsx
+++ src/containers/comics/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Index = () => {
+<<<<<<< HEAD
   return <p>This is comic list</p>;
+=======
+  return <p>Ini ngerubah, jangan bilang bilang</p>;
+>>>>>>> merubah index.jsx
 };
 
 export default Index;
